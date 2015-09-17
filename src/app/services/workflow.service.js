9e65ece4c35403abc28522ec026e18964a0b0e27
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-09-17 16:47:32 -0500

    refactoring workflow service from ptero perk-sdk

--- src/app/services/workflow.service.js
+++ src/app/services/workflow.service.js
@@ -22,11 +22,11 @@
       var deferred = $q.defer();
       $q.all({
         skeleton: Reports.get({reportType: 'workflow-skeleton', workflow_id: workflowId}),
-        details: Reports.get({reportType: 'workflow-details', workflow_id: workflowId}),
-        outputs: Reports.get({reportType: 'workflow-outputs', workflow_id: workflowId}),
+        // details: Reports.get({reportType: 'workflow-details', workflow_id: workflowId}),
+        //outputs: Reports.get({reportType: 'workflow-outputs', workflow_id: workflowId}),
         executions: Reports.get({reportType: 'workflow-executions', workflow_id: workflowId}),
-        status: Reports.get({reportType: 'workflow-status', workflow_id: workflowId}),
-        submissionData: Reports.get({reportType: 'workflow-submission-data', workflow_id: workflowId})
+        //status: Reports.get({reportType: 'workflow-status', workflow_id: workflowId}),
+        //submissionData: Reports.get({reportType: 'workflow-submission-data', workflow_id: workflowId})
       })
         .then(function(result) {
           $log.info('Workflow Service $q.all complete.');
@@ -41,128 +41,72 @@
     }
 
     function parseWorkflow(result) {
-      var skeleton = result.skeleton;
-      var executions = parseExecutions(result.executions.executions);
-      var rootExecution = _.find(executions.tasks, 'id', skeleton.rootTaskId);
-      var workflow = [{
-        id: skeleton.id,
-        depth: 0,
-        type: 'workflow',
-        name: skeleton.name,
-        status: skeleton.status,
-        timestamp: new Date(getTimestampForStatusAndHistory(skeleton.status, rootExecution.statusHistory)).toISOString(),
-        nodes: getTaskNodes(skeleton.tasks, executions, 1, null)
-      }];
-
-      return workflow;
+      var workflow = result.skeleton;
+      var executions = result.executions;
+      return {
+        id: workflow.id,
+        name: workflow.name,
+        rootTaskId: workflow.rootTaskId,
+        begins: workflow.begins,
+        status: workflow.status,
+        //executions: parseExecutions(workflow, executions),
+        //methods: parseMethods(workflow, executions),
+        tasks: parseTasks(workflow.tasks, executions)
+      };
     }
 
-    function getTaskNodes(tasks, executions, depth, parentColor) {
-      tasks = _.sortBy(tasks, 'topologicalIndex');
-      var taskNodes = _.map(tasks, function(task) {
-        var taskExecutions = _.select(executions.tasks, { id: task.id, parentColor: parentColor });
-        // for each task node, return an array of task execution status objects.
-        function iterator(task, depth, execution, index, tasks) {
-          return {
-            id: execution.id,
-            depth: depth,
-            type: 'task',
-            name: 'Task' + execution.id,
-            status: execution.status,
-            timestamp: getTimestampForStatusAndHistory(execution.status, execution.statusHistory),
-            nodes: getMethodNodes(task, tasks, depth, execution, index)
-          };
-        }
-        var getNodes = _.partial(iterator, task, depth);
-        return _.map(taskExecutions, function(execution, index, tasks) {
-          return getNodes(execution, index, tasks);
-        });
-      });
-      return taskNodes;
-    }
+    function parseTasks(tasks, executions) {
+      return _.map(tasks, function(task, key) {
+        $log.debug('parsing task');
+        $log.debug(task);
 
-    function getMethodNodes(task, depth, execution, index) {
-      return ['method node1', 'method node2'];
-    }
-
-    function getStatusInfoRowsForTaskExecution(name, task, taskExecution, executions, nestingLevel, index) {
-      var taskNodes = [];
-      taskNodes.push({
-        id: String(String(nestingLevel) + String(index)),
-        name: name,
-        status: taskExecution.status,
-        timestamp: getTimestampForStatusAndHistory(taskExecution.status, taskExecution.statusHistory),
-        nestingLevel: nestingLevel,
-        type: 'task',
-        methods: parseMethods(task, taskExecution, executions, nestingLevel + 1)
-      });
-      return taskNodes;
-    }
-
-    function parseMethods(task, execution, indexedExecutions, nestingLevel) {
-      var methods = _.map(task.methods, function(method) {
-        $log.info(['parsing method: ', JSON.stringify(method)].join(' '));
-        return getStatusInfoRowsForMethod(method, execution.color, indexedExecutions, nestingLevel);
-      });
-      return methods;
-    }
-
-    function getStatusInfoRowsForMethod(method, color, indexedExecutions, nestingLevel) {
-      var statusInfo = {},
-        executions = indexedExecutions.methods[method.id];
-//      $log.info(['getting status info for method: ', JSON.stringify(method)].join(' '));
-      executions
-        .filter(function (e) {
-          return e.parentColor == color;
-        })
-        .forEach(function (execution, index, executions) {
-          if (statusInfo === undefined) { var statusInfo = {}; }
-          statusInfo = {
-            name: method.name,
-            status: execution.status,
-            timestamp: getTimestampForStatusAndHistory(execution.status, execution.statusHistory),
-            nestingLevel: nestingLevel,
-            type: method.service,
-          };
-          if (method.service == "workflow") {
-            statusInfo.nodes = getTaskNodes(method.parameters.tasks, indexedExecutions, nestingLevel + 1, execution.color);
-          }
-        });
-      return statusInfo;
-    }
-
-    function getSortedTaskKeys(tasks) {
-      $log.info('sorting keys for tasks: '+ JSON.stringify(tasks));
-      return Object.keys(tasks).sort(function(a, b) {
-        return tasks[a].topologicalIndex - tasks[b].topologicalIndex;
+        return {
+          id: task.id,
+          name: key,
+          parallelBy: task.parallelBy,
+          topologicalIndex: task.topologicalIndex,
+          executions: [],
+          methods: parseMethods(task.methods, executions)
+        };
       });
     }
 
-    function getTimestampForStatusAndHistory(status, history) {
-      var statuses = history.filter(function (statusUpdate) {
-        return statusUpdate.status == status;
+    function parseMethods(methods, executions) {
+      return _.map(methods, function(method) {
+        $log.debug('parsing method');
+        $log.debug(method);
+        // there exists four method types that are either Methods or DAGs:
+        // job: Method,
+        // workflow: DAG,
+        // workflow-block: Method,
+        // workflow-converge Method
+
+        return {
+          id: method.id,
+          name: method.name,
+          service: method.service,
+          serviceUrl: method.serviceUrl ? method.serviceUrl : null,
+          executions: []
+        };
       });
-      return statuses[0].timestamp;
     }
-
-    function parseExecutions(executions) {
-      var indexedExecutions = { methods: [], tasks: [] };
-      _.each(executions, function(execution) {
-        if(_.has(execution, 'taskId')) {
-          execution.type = 'task';
-          execution.id = execution.taskId;
-          indexedExecutions.tasks.push(execution)}
-        else if(_.has(execution, 'methodId')) {
-          execution.type = 'method';
-          execution.id = execution.methodId;
-          indexedExecutions.methods.push(execution)
-        }
-        else { console.error('indexExecutions encountered unknown execution type "' + JSON.stringify(execution)); }
-      });
-
-      return indexedExecutions;
-    }
-
   }
-
+//  my ($class, $hashref) = @_;
+//
+//  my $self = bless {}, $class;
+//    $self->{id} = $hashref->{id};
+//    $self->{root_task_id} = $hashref->{rootTaskId};
+//    $self->{name} = $hashref->{name};
+//    $self->{status} = $hashref->{status};
+//    $self->{method_index} = {};
+//    $self->{task_index} = {};
+//
+//  while (my ($key, $val) = each %{$hashref->{tasks}}) {
+//    $self->{tasks}{$key} = Ptero::Concrete::Workflow::Task->new(
+//    $val, $key);
+//}
+//
+//  $self->register_components();
+//
+//return $self;
 })();
