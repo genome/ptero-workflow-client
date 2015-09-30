8ce818c484c512875e9dbedf3e2faed4ad258e54
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-09-30 11:15:16 -0500

    converted reportTree to use bindToController pattern

--- src/app/views/workflow/directives/reportTree.directive.js
+++ src/app/views/workflow/directives/reportTree.directive.js
@@ -1,30 +1,42 @@
 (function() {
+  'use strict';
   angular.module('pteroWorkflowClient.views')
-    .directive('reportTree', function($compile, $templateCache) {
-      return {
-        restrict: 'E',
-        scope: {
-          val: '=',
-          parentData: '='
-        },
-        templateUrl: 'app/views/workflow/directives/reportTree.html',
-        link: function(scope, el, attrs) {
-          scope.isParent = angular.isArray(scope.val.items);
-          scope.delSubtree = function() {
-            if(scope.parentData) {
-              scope.parentData.splice(
-                scope.parentData.indexOf(scope.val),
-                1
-              );
-            }
-            scope.val={};
-          };
-          el.replaceWith(
-            $compile(
-              $templateCache.get('recursive.html')
-            )(scope)
-          );
-        }
-      };
-    });
+    .directive('reportTree', reportTree)
+    .controller('ReportTreeController', ReportTreeController);
+
+  /* @ngInject */
+  function reportTree($compile, $templateCache) {
+    return {
+      restrict: 'E',
+      scope: {
+        val: '=',
+        parentData: '='
+      },
+      bindToController: true,
+      controller: 'ReportTreeController',
+      controllerAs: 'vm',
+      templateUrl: 'app/views/workflow/directives/reportTree.html',
+      link: function(scope, el, attrs) {
+        scope.vm.isParent = angular.isArray(scope.vm.val.items);
+        scope.vm.delSubtree = function() {
+          if(scope.vm.parentData) {
+            scope.vm.parentData.splice(
+              scope.vm.parentData.indexOf(scope.vm.val),
+              1
+            );
+          }
+          scope.vm.val={};
+        };
+        el.replaceWith(
+          $compile($templateCache.get('recursive.html'))(scope)
+        );
+      }
+    };
+  }
+
+  /* @ngInject */
+  function ReportTreeController($log) {
+    $log.debug('ReportTreeController loaded.');
+    var vm = this;
+  }
 })();
