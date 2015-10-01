86360e5aee70618f16f8270303342ffb8a43f528
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-10-01 13:45:15 -0500

    example timeline rendering properly

--- src/app/views/workflow/directives/executionTimeline.directive.js
+++ src/app/views/workflow/directives/executionTimeline.directive.js
@@ -25,15 +25,142 @@
   }
 
   /* @ngInject */
-  function ExecutionTimelineController(d3) {
+  function ExecutionTimelineController($scope, $element, d3) {
+    function randomData() {
+
+      var addToLane = function (chart, item) {
+        var name = item.lane;
+
+        if (!chart.lanes[name])
+          chart.lanes[name] = [];
+
+        var lane = chart.lanes[name];
+
+        var sublane = 0;
+        while(isOverlapping(item, lane[sublane]))
+          sublane++;
+
+        if (!lane[sublane]) {
+          lane[sublane] = [];
+        }
+
+        lane[sublane].push(item);
+      };
+
+      var isOverlapping = function(item, lane) {
+        if (lane) {
+          for (var i = 0; i < lane.length; i++) {
+            var t = lane[i];
+            if (item.start < t.end && item.end > t.start) {
+              return true;
+            }
+          }
+        }
+        return false;
+      };
+
+      var parseData = function (data) {
+        var i = 0, length = data.length, node;
+        var chart = { lanes: {} };
+
+        for (i; i < length; i++) {
+          var item = data[i];
+
+          addToLane(chart, item);
+
+
+        }
+
+        return collapseLanes(chart);
+      };
+
+      var collapseLanes = function (chart) {
+        var lanes = [], items = [], laneId = 0;
+        var now = new Date();
+
+        for (var laneName in chart.lanes) {
+          var lane = chart.lanes[laneName];
+
+          for (var i = 0; i < lane.length; i++) {
+            var subLane = lane[i];
+
+            lanes.push({
+              id: laneId,
+              label: i === 0 ? laneName : ''
+            });
+
+            for (var j = 0; j < subLane.length; j++) {
+              var item = subLane[j];
+
+              items.push({
+                id: item.id,
+                lane: laneId,
+                start: item.start,
+                end: item.end,
+                class: item.end > now ? 'future' : 'past',
+                desc: item.desc
+              });
+            }
+
+            laneId++;
+          }
+        }
+
+        return {lanes: lanes, items: items};
+      }
+
+      var randomNumber = function(min, max) {
+        return Math.floor(Math.random(0, 1) * (max - min)) + min;
+      };
+
+      var generateRandomWorkItems = function () {
+        var data = [];
+        var laneCount = randomNumber(5,7)
+          , totalWorkItems = randomNumber(20,30)
+          , startMonth = randomNumber(0,1)
+          , startDay = randomNumber(1,28)
+          , totalMonths = randomNumber(4,10);
+
+        for (var i = 0; i < laneCount; i++) {
+          var dt = new Date(2012, startMonth, startDay);
+          for (var j = 0; j < totalWorkItems; j++) {
+
+            var dtS = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + randomNumber(1,5), randomNumber(8, 16), 0, 0);
+
+            var dateOffset =  randomNumber(0,7);
+            var dt = new Date(dtS.getFullYear(), dtS.getMonth(), dtS.getDate() + dateOffset, randomNumber(dateOffset === 0 ? dtS.getHours() + 2 : 8, 18), 0, 0);
+
+            var workItem = {
+              id: i * totalWorkItems + j,
+              name: 'work item ' + j,
+              lane: 'lane ' + i,
+              start: dtS,
+              end: dt,
+              desc: 'This is a description.'
+            };
+
+            data.push(workItem);
+          }
+        }
+        return data;
+      };
+
+      return parseData(generateRandomWorkItems());
+    };
+
+    var target = $element[0].querySelector('#timeline');
+
+    var targetWidth = target.offsetWidth;
+    var targetHeight = target.offsetHeight;
+
     var data = randomData()
       , lanes = data.lanes
       , items = data.items
       , now = new Date();
 
     var margin = {top: 20, right: 15, bottom: 15, left: 60}
-      , width = 960 - margin.left - margin.right
-      , height = 500 - margin.top - margin.bottom
+      , width = targetWidth - margin.left - margin.right
+      , height = targetHeight - margin.top - margin.bottom
       , miniHeight = lanes.length * 12 + 50
       , mainHeight = height - miniHeight - 50;
 
@@ -47,7 +174,7 @@
     var y1 = d3.scale.linear().domain([ext[0], ext[1] + 1]).range([0, mainHeight]);
     var y2 = d3.scale.linear().domain([ext[0], ext[1] + 1]).range([0, miniHeight]);
 
-    var chart = d3.select('body')
+    var chart = d3.select(target)
       .append('svg:svg')
       .attr('width', width + margin.right + margin.left)
       .attr('height', height + margin.top + margin.bottom)
@@ -312,127 +439,5 @@
       return result;
     }
   }
-
-  var RandomData = function() {
-
-    var addToLane = function (chart, item) {
-      var name = item.lane;
-
-      if (!chart.lanes[name])
-        chart.lanes[name] = [];
-
-      var lane = chart.lanes[name];
-
-      var sublane = 0;
-      while(isOverlapping(item, lane[sublane]))
-        sublane++;
-
-      if (!lane[sublane]) {
-        lane[sublane] = [];
-      }
-
-      lane[sublane].push(item);
-    };
-
-    var isOverlapping = function(item, lane) {
-      if (lane) {
-        for (var i = 0; i < lane.length; i++) {
-          var t = lane[i];
-          if (item.start < t.end && item.end > t.start) {
-            return true;
-          }
-        }
-      }
-      return false;
-    };
-
-    var parseData = function (data) {
-      var i = 0, length = data.length, node;
-      var chart = { lanes: {} };
-
-      for (i; i < length; i++) {
-        var item = data[i];
-
-        addToLane(chart, item);
-
-
-      }
-
-      return collapseLanes(chart);
-    };
-
-    var collapseLanes = function (chart) {
-      var lanes = [], items = [], laneId = 0;
-      var now = new Date();
-
-      for (var laneName in chart.lanes) {
-        var lane = chart.lanes[laneName];
-
-        for (var i = 0; i < lane.length; i++) {
-          var subLane = lane[i];
-
-          lanes.push({
-            id: laneId,
-            label: i === 0 ? laneName : ''
-          });
-
-          for (var j = 0; j < subLane.length; j++) {
-            var item = subLane[j];
-
-            items.push({
-              id: item.id,
-              lane: laneId,
-              start: item.start,
-              end: item.end,
-              class: item.end > now ? 'future' : 'past',
-              desc: item.desc
-            });
-          }
-
-          laneId++;
-        }
-      }
-
-      return {lanes: lanes, items: items};
-    }
-
-    var randomNumber = function(min, max) {
-      return Math.floor(Math.random(0, 1) * (max - min)) + min;
-    };
-
-    var generateRandomWorkItems = function () {
-      var data = [];
-      var laneCount = randomNumber(5,7)
-        , totalWorkItems = randomNumber(20,30)
-        , startMonth = randomNumber(0,1)
-        , startDay = randomNumber(1,28)
-        , totalMonths = randomNumber(4,10);
-
-      for (var i = 0; i < laneCount; i++) {
-        var dt = new Date(2012, startMonth, startDay);
-        for (var j = 0; j < totalWorkItems; j++) {
-
-          var dtS = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + randomNumber(1,5), randomNumber(8, 16), 0, 0);
-
-          var dateOffset =  randomNumber(0,7);
-          var dt = new Date(dtS.getFullYear(), dtS.getMonth(), dtS.getDate() + dateOffset, randomNumber(dateOffset === 0 ? dtS.getHours() + 2 : 8, 18), 0, 0);
-
-          var workItem = {
-            id: i * totalWorkItems + j,
-            name: 'work item ' + j,
-            lane: 'lane ' + i,
-            start: dtS,
-            end: dt,
-            desc: 'This is a description.'
-          };
-
-          data.push(workItem);
-        }
-      }
-      return data;
-    };
-
-    return parseData(generateRandomWorkItems());
-  };
 
 })();
